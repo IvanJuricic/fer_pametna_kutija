f89273edc78ecf7e01eb389219e653b738bf75a3
borna-blazevic <borna.blazevic@fer.hr>
2020-02-26 12:21:53 +0100

    add load cell support

---
+++ src/models/load_cell2.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const dataSchema = new Schema({
+    mass: {
+        type: Number,
+        index: true,
+        required: true,
+    },
+    user: mongoose.model('User').schema,
+},
+    { timestamps: { createdAt: 'created_at' } });
+
+const Data = mongoose.model('LD2', dataSchema);
+module.exports = Data;
